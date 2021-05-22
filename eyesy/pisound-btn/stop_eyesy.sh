12c99b5c7a5ee944563f8b50ad429c856ba11618
Alessandro <68548501+jqrsound@users.noreply.github.com>
2021-05-22 18:27:06 +0000

    Create stop_eyesy.sh

---
+++ eyesy/pisound-btn/stop_eyesy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+. /usr/local/pisound/scripts/common/common.sh
+flash_leds 100
+
+#!/bin/bash
+#amixer cset numid=11 off
+
+systemctl stop eyesy-python.service
+systemctl stop eyesy-web.service
+systemctl stop eyesy-web-socket.service
+systemctl stop eyesy-pd.service
